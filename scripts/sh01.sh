c934cca25e1ee1e5b685a78d7f3c625438e86e8f
Ｖ字龍(Vdragon) <pika1021@gmail.com>
2014-06-02 06:58:02 +0800

    翻譯 sh01
    Signed-off-by: Ｖ字龍(Vdragon) <pika1021@gmail.com>

--- scripts/sh01.sh
+++ scripts/sh01.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-# Program:
-#       This program shows "Hello World!" in your screen.
-# History:
+# 程式：
+#       本程式將會顯示 "GNU Bash 世界你好！" 在您的螢幕上。
+# 變更紀錄：
 # 2005/08/23	VBird	First release
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
-echo -e "Hello World! \a \n"
+echo -e "GNU Bash 世界你好！\a \n"
 exit 0
