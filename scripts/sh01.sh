b46e65e3a252e0bfdfd1dc539aad6595c466fb1b
Ｖ字龍(Vdragon) <pika1021@gmail.com>
2014-06-02 07:14:31 +0800

    加上變更紀錄

--- scripts/sh01.sh
+++ scripts/sh01.sh
@@ -2,6 +2,7 @@
 # 程式：
 #       本程式將會顯示 "GNU Bash 世界你好！" 在您的螢幕上。
 # 變更紀錄：
+# 2014/06/01	Ｖ字龍	翻譯為正體中文版本
 # 2005/08/23	VBird	First release
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
